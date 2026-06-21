c86c8e9eac23ec6d817c129ff129a9e92347c575
Juan Albarran <jalbarrandev@gmail.com>
2026-06-21 11:24:04 -0400

    chore: removing unused packages

--- packages/impeccable/extensions/impeccable/index.ts
+++
@@ -1,9 +0,0 @@
-import type { ExtensionAPI } from '@earendil-works/pi-coding-agent';
-
-import { registerDetectTool } from './commands/detect-tool';
-import { registerImpeccableCommand } from './commands/impeccable';
-
-export default function impeccableExtension(pi: ExtensionAPI) {
-  registerDetectTool(pi);
-  registerImpeccableCommand(pi);
-}
--- packages/stacked-prs/extensions/stacked-prs/index.ts
+++
@@ -1,6 +0,0 @@
-import type { ExtensionAPI } from '@earendil-works/pi-coding-agent';
-import { registerStackedPrsCommands } from './commands';
-
-export default function stackedPrsExtension(pi: ExtensionAPI) {
-  registerStackedPrsCommands(pi);
-}
